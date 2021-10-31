2c5ed64c1b7a97cb905726876772db53f0a40aa5
Eduardo <eduardo@purplemetrics.com.br>
2021-10-31 02:49:10 -0300

    feat: first commit

---
+++ pgntradutor.js
@@ -0,0 +1,24 @@
+function translate() {
+    const from = document.getElementById('from')
+    const to = document.getElementById('to')
+
+    let fromString = from.value
+    let isBetweenBrackets = false
+    for (i=0; i<fromString.length; i++) {
+        let char = fromString.charAt(i)
+        if (char === '[') {
+            isBetweenBrackets = true
+        } else if (char === ']') {
+            isBetweenBrackets = false
+        } else if (!isBetweenBrackets) {
+            char = char
+                .replace('R', 'K')
+                .replace('T', 'R')
+                .replace('C', 'N')
+                .replace('D', 'Q')
+            fromString = fromString.substring(0, i) + char + fromString.substring(i + 1)
+        }
+    }
+
+    to.value = fromString
+}
